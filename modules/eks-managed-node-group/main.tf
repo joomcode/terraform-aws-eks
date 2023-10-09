978887b2e46a1ba1defa10dff7534cbfb87125bc
Alexey Miasoedov <amiasoedov@joom.com>
2023-10-10 00:33:58 +0300

    use postcondition to prevent destruction

--- modules/eks-managed-node-group/main.tf
+++ modules/eks-managed-node-group/main.tf
@@ -379,7 +379,11 @@ resource "aws_eks_node_group" "this" {
 
   lifecycle {
     create_before_destroy = true
-    prevent_destroy       = true
+    # prevent_destroy       = true
+    postcondition {
+      condition     = false
+      error_message = "Node group should not be destroyed yet."
+    }
     ignore_changes = [
       scaling_config[0].desired_size,
     ]
